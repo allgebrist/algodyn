3672f805d58c559a2d49c51d2c2499261fbf7060
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-09 00:23:37 +0100

    updated plot_info_signature

--- R/plot_info_signature.R
+++ R/plot_info_signature.R
@@ -1,5 +1,23 @@
 
-plot_info_signature <- function() {
+plot_info_signature <- function(x, block_size = 4, offset = 4) {
 
-    return()
+    is <- info_signature(x, block_size, offset)
+
+    diffs <- c()
+
+    for(i in 1:nrow(is)) {
+        if(i != nrow(is)) {
+            diffs <- c(diffs, is$bdm_difference[i]-is$bdm_difference[i+1])
+        }
+    }
+
+    plot(diffs, xlab = "edges sorted by max info value", ylab = "sequential info differences",
+         main = "Cutting points plot", col = "blue")
+
+    lines(diffs, col = "blue")
+
+    curve(log2(2)*x^0, col = "purple", add = TRUE)
+
+    legend("topright", "log(2)", lty = 1, col = "purple")
+
 }
