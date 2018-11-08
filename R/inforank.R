007bd026e6eb5550e7e6ce6cee214e70c6dcb4fd
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-08 11:35:25 +0100

    modified inforank

--- R/inforank.R
+++ R/inforank.R
@@ -1,5 +1,7 @@
 
-inforank <- function(x) {
+inforank <- function(x, what = 'default', block_size = 4, offset = 4) {
+
+    inforank_df <- c()
 
     if(is.igraph(x) || is.matrix(x)) {
         x_adj_matrix <- c()
@@ -9,14 +11,28 @@ inforank <- function(x) {
             x_adj_matrix <- x
         }
 
-        # Include here perturbations on graphs
+        # Perturbations on graphs
+        if(what = 'vertices') {
+            inforank_df <- calculate_info_vertices(x, block_size, offset)
+            inforank_df$inforank <- rank(-as.numeric(inforank_df$bdm_difference), ties.method="min")
+        } else if(what = 'edges' || what = 'default') {
+            inforank_df <- calculate_info_edges(x, block_size, offset)
+            inforank_df$inforank <- rank(-as.numeric(inforank_df$bdm_difference), ties.method="min")
+        } else {
+            stop("The ranking should be performed according to the contributions of either vertices or edges.")
+        }
 
     } else if(is.character(x)) {
 
         # Include here perturbations on strings
+        if(what = 'default') {
+            # Remove characters
+        } else {
+            stop("Only characters can be removed from the string. Please leave the 'what' parameter unaltered.")
+        }
 
     } else {
-        stop("The object should be either a graph, adjacency matrix or string")
+        stop("The object should be either a graph, adjacency matrix or string.")
     }
 
     return(inforank_df)
