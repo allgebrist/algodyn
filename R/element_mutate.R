0d960c97e1cbbe3220196bec89f334618beef1b6
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-11 21:54:50 +0100

    added instructions for mutate function

--- R/element_mutate.R
+++ R/element_mutate.R
@@ -1,5 +1,9 @@
 
 element_mutate <- function() {
+    ## a element_mutate le das una coordenada del objeto que quieres mutar,
+    ## si es binario le hace flip, si no lo es entonces necesita un input o
+    ## lo hace aleatorio. Es básicamente la función que usarías para las
+    ## perturbaciones para calculate_info_nodes y la misma para calculate_info_edges.
 
-    return()
+    return(mutated_string)
 }
