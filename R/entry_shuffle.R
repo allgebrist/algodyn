ab78dd1cfd324bdb89347ff9fbb6145b4e46a2a3
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-14 14:24:54 +0100

    updated row and entry shuffle

--- R/entry_shuffle.R
+++ R/entry_shuffle.R
@@ -4,5 +4,6 @@ entry_shuffle <- function(x, row_position, column_position, replacement = NULL) 
     if (!is.null(replacement)) {
         x[row_position, column_position] <- replacement
     }
+
     return(x)
 }
--- R/row_shuffle.R
+++ R/row_shuffle.R
@@ -4,11 +4,10 @@ row_shuffle <- function(x, row_position, replacement = NULL) {
     if (!is.null(replacement)) {
         if(length(replacement) == nrow(x)) {
             x[row_position, ] <- replacement
-            return(x)
         } else {
             stop("ERROR: The inserted row should have as many columns as the matrix")
         }
-    } else {
-        return(x)
     }
+
+    return(x)
 }
