598738b8f8e1b105060bf0ab9e0efd48bf3c5ed5
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-14 10:03:29 +0100

    added calculate_info_bits

---
+++ R/calculate_info_bits.R
@@ -0,0 +1,25 @@
+
+calculate_info_bits <- function(x, block_size, offset, base) {
+
+    k_values <- get_k_values(alphabet_size = base)
+    x_bdm_value <- bdm1d(x, block_size, offset, base, k_values)
+    x_char_vector <- unlist(str_split(x, pattern = ""))
+
+    perturbations <- c()
+
+    for (i in 1:length(x_char_vector)) {
+        index <- !logical(length(x_char_vector))
+        index[i] <- FALSE
+
+        back <- paste(x_char_vector[index], sep = "", collapse = "")
+        perturbations <- c(perturbations, back)
+    }
+
+    perturbations_bdm_values <- unlist(lapply(perturbations, bdm1d, block_size, offset, base, k_values))
+
+    bdm_differences <- x_bdm_value - perturbations_bdm_values
+
+    bdm_df <- data.frame(perturbations = perturbations, bdm_differences = bdm_differences, stringsAsFactors = FALSE)
+
+    return(bdm_df)
+}
