9f42e64cfab8dab6a063e11e567d9467659c72e0
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-18 09:24:27 +0100

    updated info_signature

--- R/info_signature.R
+++ R/info_signature.R
@@ -6,13 +6,15 @@ info_signature <- function(x, what, block_size = 4, offset = 4) {
         # display elements with bdm_difference > 0 (information loss)
         edge_deletions_df <- edge_deletions_df[edge_deletions_df$bdm_difference > 0, ]
         edge_deletions_df <- edge_deletions_df[order(-edge_deletions_df$bdm_difference), ]
+        signature <- edge_deletions_df
     } else if (what = 'vertices') {
         vertex_deletions_df <- calculate_info_vertices(x, block_size, offset)
         vertex_deletions_df <- vertex_deletions_df[vertex_deletions_df$bdm_difference > 0, ]
         vertex_deletions_df <- vertex_deletions_df[order(-vertex_deletions_df$bdm_difference), ]
+        signature <- vertex_deletions_df
     } else {
         stop("ERROR: A valid 'what' parameter should be given (edges or vertices).")
     }
 
-    return(edge_deletions_df)
+    return(signature)
 }
