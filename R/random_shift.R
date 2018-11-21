0e07ab864b9eeae19c161836f8908ebe1f61edc5
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-21 23:01:53 +0100

    updated random_shift

--- R/random_shift.R
+++ R/random_shift.R
@@ -1,7 +1,16 @@
 
-random_shift <- function(x) {
+random_shift <- function(x, position, elements) {
 
-    # call element_mutate
+    if (is.matrix(x)) {
+        set.seed(123)
+        if (missing(elements)) {
+            elements <- c(0, 1)
+        } else {
+            elements <- elements
+        }
+        new_row <- sample(elements, size = ncol(x), replace = TRUE)
+        x[position, ] <- new_row
+    }
 
     return()
 }
