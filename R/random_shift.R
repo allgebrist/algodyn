271675cde8a2c55de3183a948e9a0f50fcae48d6
Allan A. Zea <allan.zea@ciens.ucv.ve>
2018-11-22 11:25:51 +0100

    updated random_shift

--- R/random_shift.R
+++ R/random_shift.R
@@ -1,15 +1,20 @@
 
 random_shift <- function(x, position, seed, elements) {
 
+    set.seed(seed)
+
+    if (missing(elements)) {
+        elements <- c(0, 1)
+    }
+
     if (is.matrix(x)) {
-        set.seed(seed)
-        if (missing(elements)) {
-            elements <- c(0, 1)
-        } else {
-            elements <- elements
-        }
         new_row <- sample(elements, size = ncol(x), replace = TRUE)
         x[position, ] <- new_row
+    } else if (is.character(x)) {
+        new_characters <- sample(elements, size = length(position), replace = TRUE)
+        x[position] <- new_characters
+    } else {
+        stop("ERROR: The object should be either a graph, adjacency matrix or string.")
     }
 
     return(x)
