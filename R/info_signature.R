a6d19272ae94139b76e239f256a270fc2d23bb84
Allan A. Zea <allan.zea@ciens.ucv.ve>
2019-04-03 18:50:10 +0200

    updated roxygen templates

--- R/info_signature.R
+++ R/info_signature.R
@@ -8,13 +8,22 @@
 #' @param block_size block size required to estimate the Kolmogorov-Chaitin complexity of \code{x} using the 2-dimensional Block Decomposition Method (BDM).
 #' @param offset offset required to estimate the Kolmogorov-Chaitin complexity of \code{x} using the 2-dimensional Block Decomposition Method (BDM).
 #'
-#' @details The information signature \eqn{\sigma(G)} of graph \eqn{G} is the distribution of information values for either the edges or vertices of \eqn{G}.
+#' @details The information signature \eqn{\sigma(G)} of graph \eqn{G} is the distribution of information values for either the edges or vertices of \eqn{G}, sorted decreasingly by information contribution.
+#' This function considers only those elements (vertices or edges) that have a positive information contribution (those whose removal from \eqn{G} leads to a loss of information).
+#' To obtain the complete distribution of information values (i.e. including negative and neutral contributions), it suffices to use \code{info_spectra} instead.
 #'
 #' @import igraph
-#' @return
-#'
+#' @return A data frame corresponding to the information signature of the input graph.
 #'
 #' @examples
+#' \dontrun{
+#' # Compute the information signature of the Folkman graph
+#' folkman <- make_graph("Folkman")
+#' is <- info_signature(folkman)
+#'
+#' # Verify that
+#' nrow(info_signature(make_full_graph(5), what = 'vertices'))
+#' }
 #'
 #' @export
 info_signature <- function(x, what, block_size = 4, offset = 4) {
